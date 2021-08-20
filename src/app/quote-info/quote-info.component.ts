f55ded2a51fa746b64b4eda1891e42781d9cda10
ken <ken.mbira@student.moringaschool.com>
2021-08-20 10:46:40 +0300

    move the hide button to the bottom of the card

--- src/app/quote-info/quote-info.component.ts
+++ src/app/quote-info/quote-info.component.ts
@@ -9,7 +9,9 @@ import { Quote } from '../quote';
 })
 export class QuoteInfoComponent implements OnInit {
 quoteInfo : Quote[] = [
-  new Quote("Albert Einstein",1,"Physicist", 0, 0,"The important thing is to not stop questioning. Curiosity has its own reason for existing.",new Date(1920,3,2))
+  new Quote("Albert Einstein",1,"Theoretical Physicist", 0, 0,"The important thing is to not stop questioning. Curiosity has its own reason for existing.",new Date(1920,3,2)),
+  new Quote("Elon Musk",2,"CEO of Tesla Motors", 0, 0, "When something is important enough, you do it even if the odds are not in your favour.",new Date(2015,3,6))
+
 ];
 toggleDetails(index){
 this.quoteInfo[index].showDetails = !this.quoteInfo[index].showDetails;
