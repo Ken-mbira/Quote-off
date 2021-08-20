0348982e4150f280588eb36ee13bcde9fe161990
ken <ken.mbira@student.moringaschool.com>
2021-08-20 14:14:57 +0300

    add a form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteInfoComponent } from './quote-info/quote-info.component';
-import { RemoveQuoteComponent } from './remove-quote/remove-quote.component'
+import { RemoveQuoteComponent } from './remove-quote/remove-quote.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteInfoComponent,
-    RemoveQuoteComponent
+    RemoveQuoteComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
